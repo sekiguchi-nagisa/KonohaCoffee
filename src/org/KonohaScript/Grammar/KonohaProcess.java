4a8ffe00d8b4b43fd01c01acf5a25ad1b69d9ef0
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2013-07-05 17:32:27 +0900

    add KonohaProcessMonitor

--- src/org/KonohaScript/Grammar/KonohaProcess.java
+++ src/org/KonohaScript/Grammar/KonohaProcess.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class KonohaProcess {
 	private Process					proc;
@@ -18,62 +23,70 @@ public class KonohaProcess {
 	private InputStream				stdout	= null;
 	private InputStream				stderr	= null;
 
-	private String commandName;
+	private StringBuilder cmdNameBuilder;
 	private ArrayList<String>	commandList;
 	private boolean enableSyscallTrace = false;
+	public boolean isKilled = false;
+	private String errorMessage;
 	private final String logdirPath = "/tmp/strace-log";
-	private String logFilePath;
-//	private Logger logger;
+	public String logFilePath;
+	private final String log4jConfig = "log4j.properties";
 
-	public KonohaProcess() {
-		this.commandList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		KonohaProcess kProc = new KonohaProcess("ls", false);
+		kProc.setArgument("/root");
+		kProc.start();
+		//kProc.kill();
+		//System.out.println("return: " + kProc.getRet());
+		System.out.print(kProc.getStdout());
+		System.err.print(kProc.getStderr());
 	}
-
+	
 	public KonohaProcess(String command) {
+		this.cmdNameBuilder = new StringBuilder();
 		this.commandList = new ArrayList<String>();
-		this.commandList.add(command);
+		setArgument(command);
 	}
 
 	public KonohaProcess(String command, boolean enableSyscallTrace) {
+		this.cmdNameBuilder = new StringBuilder();
 		this.commandList = new ArrayList<String>();
 		this.enableSyscallTrace = enableSyscallTrace;
-
+		
 		if(this.enableSyscallTrace) {
-			this.commandName = command;
-//			this.logger = Logger.getLogger("D-Shell");
-
 			String currentLogdirPath = createLogDirectory();
 			String logNameHeader = createLogNameHeader();
 			logFilePath = new String(currentLogdirPath + "/" + logNameHeader + ".log");
-
+			
 			String[] straceCmd = {"strace", "-t", "-f", "-F", "-o", logFilePath};
 			for(int i = 0; i < straceCmd.length; i++) {
 				this.commandList.add(straceCmd[i]);
 			}
 		}
-		this.commandList.add(command);
+		setArgument(command);
 	}
 
 	private String createLogDirectory() {
 		Calendar cal = Calendar.getInstance();
 		StringBuilder pathBuilder = new StringBuilder();
-
+		
 		pathBuilder.append(logdirPath + "/");
 		pathBuilder.append(cal.get(Calendar.YEAR) + "-");
 		pathBuilder.append((cal.get(Calendar.MONTH) + 1) + "-");
 		pathBuilder.append(cal.get(Calendar.DATE));
-
+		
 		String subdirPath = pathBuilder.toString();
 		File subdir = new File(subdirPath);
 		subdir.mkdirs();
-
+		
 		return subdirPath;
 	}
 
 	private String createLogNameHeader() {
 		Calendar cal = Calendar.getInstance();
 		StringBuilder logNameHeader = new StringBuilder();
-
+		
 		logNameHeader.append(cal.get((Calendar.HOUR) + 1) + ":");
 		logNameHeader.append(cal.get(Calendar.MINUTE) + "-");
 		logNameHeader.append(cal.get(Calendar.MILLISECOND));
@@ -82,6 +95,7 @@ public class KonohaProcess {
 	}
 
 	public void setArgument(String Arg) {
+		this.cmdNameBuilder.append(Arg + " ");
 		this.commandList.add(Arg);
 	}
 
@@ -91,7 +105,7 @@ public class KonohaProcess {
 		}
 	}
 
-	public void start() {
+	public void start() {		
 		int size = this.commandList.size();
 		String[] cmd = new String[size];
 		for(int i = 0; i < size; i++) {
@@ -167,7 +181,8 @@ public class KonohaProcess {
 			}
 			catch (InterruptedException e1) {
 				e1.printStackTrace();
-			} finally {
+			} 
+			finally {
 				this.kill();
 			}
 		}
@@ -176,43 +191,87 @@ public class KonohaProcess {
 	public int getRet() {
 		return this.proc.exitValue();
 	}
-
+	
 	public void kill() {
-		this.proc.destroy();
+		try {
+			// get target pid
+			Field pidField = this.proc.getClass().getDeclaredField("pid");
+			pidField.setAccessible(true);
+			int pid = pidField.getInt(this.proc);
+			
+			// kill process
+			String[] cmds = {"kill", "-9", Integer.toString(pid)};
+			Process procKiller = new ProcessBuilder(cmds).start();
+			procKiller.waitFor();
+			this.isKilled = true;
+		} 
+		catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} 
+		catch (SecurityException e) {
+			e.printStackTrace();
+		} 
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} 
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void deleteLogFile() {
+		new File(logFilePath).delete();
 	}
 
-//	public void parseTraceLog() {
-//		System.out.println("show systemcall error!!");
-//		logger.error("error happened at " + commandName);
-//		try {
-//			BufferedReader br = new BufferedReader(new FileReader(this.logFilePath));
-//
-//			String regex1 = "^[1-9][0-9]* .+(.+) = -[1-9].+";
-//			Pattern p1 = Pattern.compile(regex1);
-//
-//			String regex2 = "^.+(.+/locale.+).+";
-//			Pattern p2 = Pattern.compile(regex2);
-//
-//			String regex3 = "(^[1-9][0-9]*)( *)([0-9][0-9]:[0-9][0-9]:[0-9][0-9])( *)(.+)";
-//			Pattern p3 = Pattern.compile(regex3);
-//
-//			Matcher m1, m2, m3;
-//
-//			String line;
-//			while((line = br.readLine()) != null){
-//				m1 = p1.matcher(line);
-//				if(m1.find()) {
-//					m3 = p3.matcher(line);
-//					logger.error(m3.replaceAll("[pid $1] [time $3] $5"));
-//				}
-//			}
-//			br.close();
-//		} catch (FileNotFoundException e) {
-//			throw new RuntimeException(e);
-//		} catch (IOException e) {
-//			throw new RuntimeException(e);
-//		}
-//	}
+	public void parseTraceLog() {
+		System.out.println("show systemcall error!!");
+		System.out.println("Error happened at \"" + cmdNameBuilder.toString() + "\"");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(this.logFilePath));
+			
+			String regex1 = "^[1-9][0-9]* .+(.+) = -[1-9].+";
+			Pattern p1 = Pattern.compile(regex1);
+			
+			String regex2 = "^.+(.+/locale.+).+";
+			Pattern p2 = Pattern.compile(regex2);
+			
+			String regex3 = "(^[1-9][0-9]*)( *)([0-9][0-9]:[0-9][0-9]:[0-9][0-9])( *)(.+)";
+			Pattern p3 = Pattern.compile(regex3);
+			
+			Matcher m1, m2, m3;
+			
+			String line;
+			while((line = br.readLine()) != null){
+				m1 = p1.matcher(line);
+				if(m1.find()) {
+					m3 = p3.matcher(line);
+					System.out.println(m3.replaceAll("[pid $1] [time $3] $5"));
+				}
+			}
+			br.close();
+			deleteLogFile();
+		} 
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} 
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void setError(String message) {
+		this.errorMessage = message;
+	}
+	
+	public String getError() {
+		return this.errorMessage;
+	}
 }
 
 class StreamGetter extends Thread {
@@ -269,6 +328,74 @@ class StreamSetter extends Thread {
 		}
 		catch (IOException e) {
 			throw new RuntimeException(e);
+		}
+	}
+}
+
+class KonohaProcessMonitor extends Thread {
+	private ArrayList<KonohaProcess> procList;
+	
+	public KonohaProcessMonitor() {
+		this.procList = new ArrayList<KonohaProcess>();
+	}
+	
+	public void setProcess(KonohaProcess kproc) {
+		this.procList.add(kproc);
+	}
+	
+	private void diagnose(KonohaProcess kproc) {	//FIXME
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(kproc.logFilePath));
+			
+			String regex1 = "^[1-9][0-9]* .+(.+) = -[1-9].+";
+			Pattern p1 = Pattern.compile(regex1);
+			
+			Matcher m1;
+			String line;
+			while((line = br.readLine()) != null){
+				m1 = p1.matcher(line);
+				if(m1.find()) {
+					// check systemcall error
+					kproc.setError("error");
+					kproc.kill();
+				}
+			}
+			br.close();	
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void throwException() throws Exception {
+		int size = procList.size();
+		for(int i = 0; i < size; i++) {
+			KonohaProcess targetProc = procList.get(i);
+			targetProc.waitFor();
+			if(targetProc.isKilled) {
+				throw new Exception(targetProc.getError());
+			} else {
+				if(targetProc.getRet() != 0) {
+					throw new Exception(targetProc.getError());
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void run() {
+		int size = procList.size();
+		for(int i = 0; i < size; i++) {
+			KonohaProcess targetProc = procList.get(i);
+			try {
+				targetProc.getRet();
+			}
+			catch (IllegalThreadStateException e) {
+				diagnose(targetProc);
+			}
 		}
 	}
 }
